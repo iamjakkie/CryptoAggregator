4bfad532d16ede842a7a20c419ec13e5849c105d
jakkie <jakubkielbasiewicz@gmail.com>
2023-02-22 21:00:02 +0100

    Init binance futures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+const FUTURES_BINANCE_URL:&str = "https://fapi.binance.com";
+
+
+async fn f_get_server_time() {
+    let path = "/fapi/v1/time";
+    let base = String::from(FUTURES_BINANCE_URL);
+
+    let url = base + path;
+
+    println!("URL: {}", url);
+
+    let res = reqwest::get(url)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    println!("RES: {}", res);
+}
+#[tokio::main]
+async fn main() {
+    let binance_api = env::var("BINANCE_API").unwrap();
+    let binance_secret = env::var("BINANCE_SECRET").unwrap();
+
+    f_get_server_time().await;
+
+
+
 }

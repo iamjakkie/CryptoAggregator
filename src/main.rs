bab66e858109e505f2d4ab84f4637d0d37174062
jakkie <jakubkielbasiewicz@gmail.com>
2023-02-24 21:17:46 +0100

    Binance futures exchange info mapping

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ struct ExchangeInfo {
     server_time: u128,
     assets: Vec<Asset>,
     symbols: Vec<Symbol>,
+    timezone: String
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -29,12 +30,79 @@ struct RateLimit {
 #[derive(Serialize, Deserialize, Debug)]
 struct Asset {
     asset: String,
-    marginAvailable: bool,
-    autoAssetExchange: Option<u32>
+    margin_available: bool,
+    auto_asset_exchange: Option<u32>
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Symbol {
+    symbol: String,
+    pair: String,
+    contract_type: ContractType,
+    delivery_date: u64,
+    onboard_date: u64,
+    status: SymbolStatus,
+    main_margin_percent: f32,
+    required_margin_percent: f32,
+    base_asset: String,
+    quote_asset: String,
+    margin_asset: String,
+    price_precision: u8,
+    quantity_precision: u8,
+    base_asset_precision: u8,
+    quote_precision: u8,
+    underlying_type: UnderlyingType,
+    underlying_sub_type: Vec<UnderlyingSubType>,
+    settle_plan: u8,
+    trigger_protect: f32,
+    filters: Vec<Filter>,
+    order_type: Vec<OrderType>,
+    time_in_force: Vec<TimeInForce>,
+    liquidation_fee: f32,
+    market_take_bound: f32
 }
 
-struct Symbol {
-    
+#[derive(Serialize, Deserialize, Debug)]
+enum TimeInForce {
+    GTC,
+    IOC,
+    FOK,
+    GTX
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum OrderType {
+    LIMIT,
+    MARKET,
+    STOP,
+    STOP_MARKET,
+    TAKE_PROFIT,
+    TAKE_PROFIT_MARKET,
+    TRAILING_STOP_MARKET
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Filter {
+    filter_type: String,
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum ContractType {
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum SymbolStatus {
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum UnderlyingType {
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum UnderlyingSubType {
+
 }
 
 async fn f_get_server_time() {
